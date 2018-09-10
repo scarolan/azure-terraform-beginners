f038b69c764339f056f0ca342ba3c561a21b1623
Lucas.DeMelo@aa.com <Lucas.DeMelo@aa.com>
2018-09-10 20:32:41 +0000

    New line end

--- files/setup.sh
+++ files/setup.sh
@@ -10,7 +10,7 @@ cat << EOM > /var/www/html/index.html
   <head><title>Meow!</title></head>
   <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
   <center><img src="http://placekitten.com/800/600"></img></center>
-  <marquee><h1>Meow World</h1></marquee>
+  <marquee><h1>Lucas World??!!!!</h1></marquee>
   </body>
 </html>
 EOM
