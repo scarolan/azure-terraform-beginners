baf5c03ff065b8738486160c453bf3f3e13feae8
00811300 <muthulakshmanan.sivakaminathan@aa.com>
2018-09-10 15:27:04 -0500

    add little info in the setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -10,7 +10,11 @@ cat << EOM > /var/www/html/index.html
   <head><title>Meow!</title></head>
   <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
   <center><img src="http://placekitten.com/800/600"></img></center>
-  <marquee><h1>Meow World</h1></marquee>
+
+    Hello from Terraform Enterprise commit
+
+  <marquee><h1>Hello American Airlines</h1></marquee>
+
   </body>
 </html>
 EOM
