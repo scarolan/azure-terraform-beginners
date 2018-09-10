3dee8a0c690188f5c5661e9373f3e1ed57f0e4ed
aacloudguy <aacloudguy@gmail.com>
2018-09-10 20:33:16 +0000

    Its a class

--- files/setup.sh
+++ files/setup.sh
@@ -7,10 +7,11 @@ apt install -y apache2 > /dev/null 2>&1
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Meower!</title></head>
   <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
   <center><img src="http://placekitten.com/800/600"></img></center>
-  <marquee><h1>Meow World</h1></marquee>
+  <marquee><h1>Meower World</h1></marquee>
+  Tom was here
   </body>
 </html>
 EOM
