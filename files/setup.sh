1a9b47488967f0e7dd5c94c1d805c45715c82e34
Buckaroo <bkrubnzi@frankencloud.net>
2018-09-10 20:29:09 +0000

    commit

--- files/setup.sh
+++ files/setup.sh
@@ -8,8 +8,8 @@ apt install -y apache2 > /dev/null 2>&1
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
-  <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
-  <center><img src="http://placekitten.com/800/600"></img></center>
+  <body style="background-image: linear-gradient(,blue,indigo);">
+  <center><img src="http://fillmurray.com/800/600"></img></center>
   <marquee><h1>Meow World</h1></marquee>
   </body>
 </html>
