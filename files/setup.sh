394e13bbdb4503ac1be9eeecc7ef6a40ddfd13dc
Sean Carolan <scarolan@gmail.com>
2018-11-10 20:24:39 -0600

    Restore kittens as a service.

--- files/setup.sh
+++ files/setup.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
   <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
-  <center><img src="http://picsum.photos/800/600"></img></center>
+  <center><img src="http://placekitten.com/800/600"></img></center>
   <marquee><h1>Meow World</h1></marquee>
   </body>
 </html>
